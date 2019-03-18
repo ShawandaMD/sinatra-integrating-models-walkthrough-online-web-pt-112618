4ce7eb16382b0606e388d5008ad42150bdb0ede2
github username <github email address>
2019-03-18 16:24:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @words = @analyzed_text.count_of_words
+    @vowels = @analyzed_text.count_of_vowels
+    @const = @analyzed_text.count_of_consonants
+    @letter = @analyzed_text.most_used_letter.map {|key, value| key.upcase}[0]
+    @number = @analyzed_text.most_used_letter.map {|key, value| value}[0]
     erb :results
   end
 end
